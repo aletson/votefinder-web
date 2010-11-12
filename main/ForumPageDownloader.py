096736c6b34f0195d5682c2c546acd2bf128b926
dgoeke <dgoeke@gmail.com>
2010-11-12 22:07:35 +0000

    ForumPageDownloader.ReplyToThread(threadId, message) implemented

--- main/ForumPageDownloader.py
+++ main/ForumPageDownloader.py
@@ -2,13 +2,19 @@ import urllib, urllib2, cookielib, re
 import cPickle as pickle
 from votefinder.main.models import CookieStore
 from django.conf import settings
+from BeautifulSoup import BeautifulSoup
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+from poster import poster
 
 class ForumPageDownloader():
     def __init__(self):
         self.cj = cookielib.CookieJar()
-        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        #self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        self.opener = poster.streaminghttp.register_openers()
+        self.opener.add_handler(urllib2.HTTPCookieProcessor(self.cj))
         self.LoadCookies()
-        
+
     def download(self, page):
         data = self.PerformDownload(page)
 
@@ -25,36 +31,36 @@ class ForumPageDownloader():
                 return None
         else:
             return None
-            
+
     def LoginToForum(self):
         data = ""
-        
+
         try:
-            usock = self.opener.open("http://forums.somethingawful.com/account.php", 
+            usock = self.opener.open("http://forums.somethingawful.com/account.php",
                                      urllib.urlencode(dict(action='login', username=settings.SA_LOGIN, password=settings.SA_PASSWORD)))
             data = usock.read()
             usock.close()
         except URLError, e:
             return False
-        
+
         if self.IsLoggedInCorrectlyPage(data):
             self.SaveCookies()
             return True
         else:
             return False
-        
+
     def IsNeedToLogInPage(self, data):
         if re.search(re.compile(r"A BRIEF OVERVIEW \(WHY DO WE CHARGE TO REGISTER\?\)"), data) == None:
             return False
         else:
             return True
-    
+
     def IsLoggedInCorrectlyPage(self, data):
         if re.search(re.compile(r"GLUE GLUEEEEE GLUUUUUUEEE,.*?GLUUUEEE GLUE GLLLUUUUUEEEEEE"), data) == None:
             return False
         else:
             return True
-        
+
     def PerformDownload(self, page):
         try:
             usock = self.opener.open(page)
@@ -67,12 +73,33 @@ class ForumPageDownloader():
     def LoadCookies(self):
         for c in CookieStore.objects.all():
             self.cj.set_cookie(pickle.loads(c.cookie.encode('utf8')))
-        
+
     def SaveCookies(self):
         all_cookies = CookieStore.objects.all()
         all_cookies.delete()
-        
+
         for cookie in self.cj:
             new_cookie = CookieStore(cookie=pickle.dumps(cookie))
             new_cookie.save()
-            
+
+    def ReplyToThread(self, thread, message):
+        getUrl = "http://forums.somethingawful.com/newreply.php?action=newreply&threadid=%s" % thread
+        postUrl = "http://forums.somethingawful.com/newreply.php?action=newreply"
+
+        data = self.download(getUrl)
+        if data is None:
+            return
+
+        soup = BeautifulSoup(data)
+
+        inputs = { 'message': message }
+        for i in soup.findAll('input', { 'value': True }):
+            inputs[i['name']] = i['value']
+
+        del inputs['preview']
+
+        datagen, headers = poster.encode.multipart_encode(inputs)
+        request = urllib2.Request(postUrl, datagen, headers)
+        result = urllib2.urlopen(request).read()
+
+        print result
