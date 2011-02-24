df03e03ba1bd5afc64ae442b48882d747694f2e6
dgoeke <dgoeke@f94fb925-796e-31da-f9b0-9c05b58cea91>
2011-02-24 23:50:23 +0000

    Add game-specific RSS/ATOM feeds, and clean up tabs/spaces in urls.py

--- trunk/main/feeds.py
+++ trunk/main/feeds.py
@@ -1,6 +1,7 @@
-from votefinder.main.models import BlogPost, GameStatusUpdate
+from votefinder.main.models import BlogPost, GameStatusUpdate, Game
 from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Atom1Feed
+from django.shortcuts import get_object_or_404
 
 class LatestRss(Feed):
 	title = "VoteFinder Updates"
@@ -56,6 +57,41 @@ class GameStatusRss(Feed):
                 return item.timestamp
 
 class GameStatusAtom(GameStatusRss):
+        feed_type = Atom1Feed
+        subtitle = GameStatusRss.description
+
+class SpecificGameStatusRss(Feed):
+        title = "VoteFinder Game Status Updates"
+        link = "http://votefinder.org/"
+        author_name = "soru"
+        feed_url = "http://votefinder.org/game_status"
+        description = "Game status updates for games tracked by VoteFinder."
+        guid = '/'
+        game = None
+
+        def get_object(self, request, slug):
+            self.game = get_object_or_404(Game, slug=slug)
+            return self.game
+
+        def items(self):
+            return GameStatusUpdate.objects.filter(game=self.game).order_by("-id")[:5]
+
+        def item_title(self, item):
+            if item.game:
+                return "[%s] %s" % (item.game.name, item.message)
+            else:
+                return item.message
+
+        def item_description(self, item):
+            return item.message
+
+        def item_link(self, item):
+                return item.url
+
+        def item_pubdate(self, item):
+                return item.timestamp
+
+class SpecificGameStatusAtom(SpecificGameStatusRss):
         feed_type = Atom1Feed
         subtitle = GameStatusRss.description
 
