53dee1e1c9dd7d2043023b236655f00552d20f9c
Andrew Letson <aletson@mainstreethost.com>
2016-12-08 13:33:10 -0500

    Fixes bug where votes would not count without Ecco Mode on

--- main/PageParser.py
+++ main/PageParser.py
@@ -96,7 +96,7 @@ class PageParser:
 				game = Game.objects.get(id=post.game)
 				playersLastVote = Vote.objects.filter(game=post.game, author=post.author).last()
 				theLastGameDay = GameDay.objects.filter(game=post.game).last()
-				if game.ecco_mode == True and playersLastVote.unvote == False and playersLastVote.post > theLastGameDay.startPost:
+				if game.ecco_mode == False or (playersLastVote.unvote == False and playersLastVote.post > theLastGameDay.startPost):
 					v.save()
 			except Game.DoesNotExist:
 				pass
