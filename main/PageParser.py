c09662653c23977cde664f002afd66af2320308e
aletson <me@ajl.io>
2016-12-08 21:14:52 -0500

    inherited a game object, I think

--- main/PageParser.py
+++ main/PageParser.py
@@ -93,7 +93,7 @@ class PageParser:
 				if v.target == None and (v.targetString.lower() == "nolynch" or v.targetString.lower() == "no lynch"):
 					v.nolynch = True
 			try:
-				game = Game.objects.get(id=post.game)
+				game = Game.objects.get(id=post.game.id)
 				playersLastVote = Vote.objects.filter(game=post.game, author=post.author).last()
 				theLastGameDay = GameDay.objects.filter(game=post.game).last()
 				if game.ecco_mode == False or (playersLastVote.unvote == False and playersLastVote.post > theLastGameDay.startPost) or (playersLastVote.unvote == True):
