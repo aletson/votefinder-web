816ab81f3bd031b141fff4370611e07c3fc5871b
velinath <FL@votefinder.org>
2016-12-28 13:36:08 +0000

    remove media.votefinder.org

--- main/models.py
+++ main/models.py
@@ -71,8 +71,8 @@ class VotecountTemplate(models.Model):
 	after_unvoted_vote	= models.CharField(max_length=256, blank=True)
 	detail_level		= models.IntegerField(choices=DETAIL_LEVEL_CHOICES, default=3)
 	hide_zero_votes		= models.BooleanField(default=False)
-	full_tick			= models.CharField(max_length=256, default="http://media.votefinder.org/t.png")
-	empty_tick			= models.CharField(max_length=256, default="http://media.votefinder.org/te.png")
+	full_tick			= models.CharField(max_length=256, default="https://votefinder.org/t.png")
+	empty_tick			= models.CharField(max_length=256, default="https://votefinder.org/te.png")
 
 	def __unicode__(self):
 		if self.system_default:
