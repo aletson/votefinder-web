24d9834d3943181964695faa9e29c21ff0c595cc
Andrew Letson <velinath@users.noreply.github.com>
2017-01-18 20:47:04 -0500

    ...
    thanks Podima

--- main/PageParser.py
+++ main/PageParser.py
@@ -96,7 +96,7 @@ class PageParser:
 				game = Game.objects.get(id=post.game.id)
 				playersLastVote = Vote.objects.filter(game=post.game, author=post.author).last()
 				theLastGameDay = GameDay.objects.filter(game=post.game).last()
-				if game.ecco_mode == False or playersLastVote == None or (theLastGameDay == None and (playersLastVote == None or playersLastVote.unvote == True or v.unvote == True)) or (playersLastVote.unvote == False and playersLastVote.post < theLastGameDay.startPost) or (playersLastVote.unvote == True) or (v.unvote == True):
+				if game.ecco_mode == False or playersLastVote == None or (theLastGameDay == None and (playersLastVote == None or playersLastVote.unvote == True or v.unvote == True)) or (playersLastVote.post < theLastGameDay.startPost) or (playersLastVote.unvote == True) or (v.unvote == True):
 					v.save()
 			except Game.DoesNotExist:
 				v.save()
