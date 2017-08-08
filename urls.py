f42b15fb79ac668449b8ac2bf1f6e1b00c6edc48
velinath <FL@votefinder.org>
2017-08-08 13:44:20 +0000

    fix wrong discussion thread, fix urls in prep for 1.10
    updates #11

--- urls.py
+++ urls.py
@@ -2,15 +2,18 @@ from django.conf.urls import *
 from django.conf import settings
 
 from django.contrib import admin
+import votefinder.vfauth.urls
+import votefinder.main.urls
+import django.views.static
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^auth/',     include('votefinder.vfauth.urls')),
-    (r'^admin/',    include(admin.site.urls)),
-    (r'',           include('votefinder.main.urls')),
-)
+urlpatterns = [
+    url(r'^auth/',     include(votefinder.vfauth.urls)),
+    url(r'^admin/',    include(admin.site.urls)),
+    url(r'',           include(votefinder.main.urls)),
+]
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.STATIC_ROOT}),
+    ]
--- vfauth/urls.py
+++ vfauth/urls.py
@@ -1,23 +1,24 @@
 from django.conf.urls import *
-from django.contrib.auth import *
-
-urlpatterns = patterns('',
-    (r'^create/*$', 'votefinder.vfauth.views.create_step_1'),
-    (r'^create/2/*$', 'votefinder.vfauth.views.create_step_2'),
-    (r'^create/done/*$', 'votefinder.vfauth.views.create_step_3'),
-    (r'^login/*$',   'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    (r'^logout/*$',  'django.contrib.auth.views.logout', {'template_name': 'logged_out.html'}),
-    (r'^password_change/*$', 'django.contrib.auth.views.password_change', {'template_name': 'password_change_form.html', 'post_change_redirect': 'done/'}),
-    (r'^password_change/done/*$', 'django.contrib.auth.views.password_change_done', {'template_name': 'password_change_done.html'}),
-    (r'^password_reset/$',
-        'django.contrib.auth.views.password_reset',
+# from django.contrib.auth import *
+import django.contrib.auth.views
+import votefinder.vfauth.views
+urlpatterns = [
+    url(r'^create/*$', votefinder.vfauth.views.create_step_1),
+    url(r'^create/2/*$', votefinder.vfauth.views.create_step_2),
+    url(r'^create/done/*$', votefinder.vfauth.views.create_step_3),
+    url(r'^login/*$',   django.contrib.auth.views.login, {'template_name': 'login.html'}),
+    url(r'^logout/*$',  django.contrib.auth.views.logout, {'template_name': 'logged_out.html'}),
+    url(r'^password_change/*$', django.contrib.auth.views.password_change, {'template_name': 'password_change_form.html', 'post_change_redirect': 'done/'}),
+    url(r'^password_change/done/*$', django.contrib.auth.views.password_change_done, {'template_name': 'password_change_done.html'}),
+    url(r'^password_reset/$',
+        django.contrib.auth.views.password_reset,
         {'post_reset_redirect' : 'done/','template_name': 'password_reset_form.html'}),
-    (r'^password_reset/done/$',
-        'django.contrib.auth.views.password_reset_done', {'template_name': 'password_reset_done.html'}),
+    url(r'^password_reset/done/$',
+        django.contrib.auth.views.password_reset_done, {'template_name': 'password_reset_done.html'}),
     url(r'^password_reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
+        django.contrib.auth.views.password_reset_confirm,
         {'post_reset_redirect' : '/password_reset/complete/', 'template_name': 'password_reset_confirm.html'}, name="password_reset_confirm"),
-    (r'^password_reset/complete/$',
-        'django.contrib.auth.views.password_reset_complete', {'template_name': 'password_reset_complete.html'}),
-)
+    url(r'^password_reset/complete/$',
+        django.contrib.auth.views.password_reset_complete, {'template_name': 'password_reset_complete.html'}),
+]
 
