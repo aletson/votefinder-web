b9e836db9fd41993b5fe5248c9588c7a5e3d9612
aletson <me@ajl.io>
2019-03-11 09:56:06 -0400

    Enable virtual environment support

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,16 @@
 import os
 import sys
 
+VENV = '/var/www/venvs/vf' + os.environ['ENVIRONMENT'] # change this to full path in env var
+PYTHON_BIN = VENV + '/bin/python'
+
+if sys.executable != PYTHON_BIN:
+      os.execl(PYTHON_BIN, PYTHON_BIN, *sys.argv)
+    
 cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(os.path.join(cwd, 'votefinder'))
+sys.path.insert(0,'{v}/lib/python2.7/site-packages'.format(v=VENV))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "votefinder.settings"
 
