a49862e97ef63e508628e39b72a6afccb8e6d805
aletson <me@ajl.io>
2020-04-29 19:46:35 -0400

    add migration version

--- sql/migration_5.sql
+++ sql/migration_5.sql
@@ -1,3 +1,4 @@
+UPDATE migration_version SET version = 5;
 alter table main_gameday add column `state` varchar(32) after `name`;
 update main_gameday set state = 'closed' where closed = 1;
 update main_gameday set state = 'started' where closed = 0;
