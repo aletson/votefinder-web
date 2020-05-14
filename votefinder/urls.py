17c80d011baa5deecc2d8cb92233081428afaa8f
aletson <me@ajl.io>
2020-05-14 18:43:17 -0400

    fix missing import

--- votefinder/urls.py
+++ votefinder/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views import static
 from votefinder.main import urls as main_urls
