9324acb262da6a920eb336b9274418d3ba42740d
aletson <me@ajl.io>
2020-05-14 08:17:32 -0400

    revert postgres stuff, but keep the created_on column

--- sql/migration_6.sql
+++ sql/migration_6.sql
@@ -1,4 +1,3 @@
 update migration_version set version = 6;
-alter table main_game add column created_on timestamp with time zone;
-update main_game set created_on = postinfo.start_post from ( select min("timestamp") as start_post, game_id from main_post group by game_id) AS postinfo WHERE postinfo.game_id = main_game.id
-update main_game set "lastUpdated" = postinfo.end_post from ( select max("timestamp") as end_post, game_id from main_post group by game_id) AS postinfo WHERE postinfo.game_id = main_game.id
+alter table main_game add column created_on datetime not null;
+update main_game set created_on = postinfo.start_post from ( select min("timestamp") as start_post, game_id from main_post group by game_id) AS postinfo WHERE postinfo.game_id = main_game.id ;
--- votefinder/settings.py
+++ votefinder/settings.py
@@ -35,14 +35,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': VF_MYSQL_NAME,
         'USER': VF_MYSQL_USER,
         'PASSWORD': VF_MYSQL_PASS,
         'HOST': VF_MYSQL_HOST,
-        'PORT': 5432,
-        'OPTIONS': {'options': '-c search_path=' + VF_MYSQL_NAME}, #  Comment if not using postgresql
-#        'OPTIONS': {'charset': 'utf8mb4'},  Uncomment if using mysql
+        'PORT': 3306,
+        'OPTIONS': {'charset': 'utf8mb4'},  Uncomment if using mysql
     }
 }
 
