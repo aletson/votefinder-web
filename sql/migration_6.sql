e6ba2b9807415c29d3d330d700714b23ae858593
aletson <me@ajl.io>
2020-05-14 08:23:07 -0400

    mysqlize query

--- sql/migration_6.sql
+++ sql/migration_6.sql
@@ -1,3 +1,3 @@
 update migration_version set version = 6;
 alter table main_game add column created_on datetime not null;
-update main_game set created_on = postinfo.start_post from ( select min("timestamp") as start_post, game_id from main_post group by game_id) AS postinfo WHERE postinfo.game_id = main_game.id ;
+UPDATE main_game JOIN (SELECT MIN(`timestamp`) AS start_post, game_id FROM main_post GROUP BY game_id) postinfo SET created_on = postinfo.start_post WHERE postinfo.game_id = main_game.id;
