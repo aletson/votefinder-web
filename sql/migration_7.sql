161e8adce96aea2a5203c0e31ed80725b71b68e8
aletson <me@ajl.io>
2020-05-18 13:09:33 -0400

    author_search is varchar

--- sql/migration_7.sql
+++ sql/migration_7.sql
@@ -4,7 +4,7 @@ ALTER TABLE main_game CHANGE threadId thread_id int,
                       CHANGE maxPages max_pages int,
                       CHANGE currentPage current_page int;
 ALTER TABLE main_post CHANGE postId post_id int,
-                      CHANGE authorSearch author_search int,
+                      CHANGE authorSearch author_search varchar(256),
                       CHANGE pageNumber page_number int;
 ALTER TABLE main_vote CHANGE targetString target_string varchar(256);
 ALTER TABLE main_gameday CHANGE dayNumber day_number int,
