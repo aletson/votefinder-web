35055ae67596f73083b3d0bec5b22a886c1581ee
aletson <me@ajl.io>
2020-07-15 21:15:20 -0400

    login's still a thing

--- votefinder/vfauth/views.py
+++ votefinder/vfauth/views.py
@@ -47,7 +47,7 @@ def validate_and_create_user(request):
     form = CreateUserForm(request.POST)
 
     if form.is_valid():
-        user = create_user(form.cleaned_data['email'], form.cleaned_data['password'])
+        user = create_user(form.cleaned_data['login'], form.cleaned_data['email'], form.cleaned_data['password'])
         user.backend = 'django.contrib.auth.backends.ModelBackend'
         login(request, user)
         player, created = Player.objects.get_or_create(defaults={'name': request.user.name})
