1dff61c1a7cd2abe634121fe494ce0df113ecb7e
aletson <me@ajl.io>
2020-07-23 19:27:47 -0400

    remove comments module

--- votefinder/settings.py
+++ votefinder/settings.py
@@ -148,7 +148,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.admin',
-    'django_comments',
     'votefinder.main',
     'votefinder.vfauth',
 )
