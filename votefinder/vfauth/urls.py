3a62bb9998def08695f1429be5c626cf09bd8591
aletson <me@ajl.io>
2021-02-08 13:35:47 -0500

    missed one

--- votefinder/vfauth/urls.py
+++ votefinder/vfauth/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('password_change', django_views.PasswordChangeView.as_view(template_name='password_change_form.html', success_url='done')),
     path('password_change/done', django_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html')),
     path('password_reset/',
-         django_views.PasswordResetView.as_view(success_url='done/', template_name='password_reset_form.html')),
+         django_views.PasswordResetView.as_view(success_url='done', template_name='password_reset_form.html')),
     path('password_reset/done',
          django_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html')),
     re_path(r'^password_reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
